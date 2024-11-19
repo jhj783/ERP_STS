aa0292c2a5873d1cd749799ffd33a3b111aa3098
2COOOO20 <2COOOO20@DESKTOP-332SC0Q>
2024-11-19 17:21:27 +0900

    차트 서비스 완성(테스트 필요)

--- erpsystem/src/main/java/com/erp/erpsystem/service/ERP_ChartsService.java
+++ erpsystem/src/main/java/com/erp/erpsystem/service/ERP_ChartsService.java
@@ -2,6 +2,7 @@ package com.erp.erpsystem.service;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,6 +29,59 @@ public class ERP_ChartsService {
 	@Autowired
 	private FinancialStatementService financialStatementService;
 	
+	
+	// 차트.재무 비율 분석 // (테스트 필요)
+	public String getFinancialRatios(LocalDateTime sDate, LocalDateTime eDate) {
+		List<Double> roeList = new ArrayList<>();
+	    List<Double> roaList = new ArrayList<>();
+
+		for (LocalDateTime[] dates :getQuartersService.getPreviousQuartersDates()) {
+			roeList.add(getReturnOnEquity(dates[0],dates[1]));
+			roeList.add(getReturnOnAssets(dates[0],dates[1]));
+		}
+		
+		Map<String, List<Double>> financialRatios = new HashMap<>();
+	    financialRatios.put("ROEs", roeList);
+	    financialRatios.put("ROAs", roaList);
+	    
+	    Gson gson = new Gson();
+	    return gson.toJson(financialRatios);
+	}	
+	// ROE(자기 자본 이익률) // 순이익x100 / ((기초자기자본+기말자기자본)/2)
+	private Double getReturnOnEquity(LocalDateTime sDate, LocalDateTime eDate) {
+		Double a=financialStatementService.getNetProfit(sDate, eDate);
+		Double b=((financialStatementService.getTotalCapital(sDate) + financialStatementService.getTotalCapital(eDate))/2);
+		
+		return a*100/b;
+	}
+	// ROA(총 자산 이익률) // 순이익x100 / ((기초총자산+기말총자산)/2)
+	private Double getReturnOnAssets(LocalDateTime sDate, LocalDateTime eDate) {
+		Double a=financialStatementService.getNetProfit(sDate, eDate);
+		Double b=(financialStatementService.getTotalAssets(sDate)+financialStatementService.getTotalAssets(eDate))/2;
+		
+		return a*100/b;
+	}
+	
+	
+	// 차트.매출액(막대)&순이익(꺾은선) // (테스트 필요)
+	public String getSalesAndNetProfit(LocalDateTime sDate, LocalDateTime eDate) {
+		List<Double> salesList = new ArrayList<>();
+		List<Double> netProfitList = new ArrayList<>();
+		
+		for (LocalDateTime[] dates :getQuartersService.getPreviousQuartersDates()) {
+			salesList.add(financialStatementService.getSales(dates[0], dates[1]));
+			netProfitList.add(financialStatementService.getNetProfit(dates[0], dates[1]));
+		}
+		
+		Map<String, List<Double>> salesAndNetProfits = new HashMap<>();
+		salesAndNetProfits.put("Sales", salesList);
+		salesAndNetProfits.put("NetProfit", netProfitList);
+	    
+	    Gson gson = new Gson();
+	    return gson.toJson(salesAndNetProfits);
+	}
+	
+	
 	// 차트.운영비용 //
 	public String getCostSummary(LocalDateTime sDate, LocalDateTime eDate) {
 	    List<String> descriptions = List.of("복리후생비", "물류비", "공과금", "임대료", "소모품비", "관리비");
@@ -44,33 +98,18 @@ public class ERP_ChartsService {
 	    Gson gson = new Gson();
 	    return gson.toJson(costSummaryMap);
 	}
-	
-	
-	// 차트.재무 비율 분석 //
-	public String getFinancialRatios(LocalDateTime sDate, LocalDateTime eDate) {
-		for ( LocalDateTime[] dates :getQuartersService.getPreviousQuartersDates()) {
-			
-		}
 		
-		return "a";
-	}	
 	
-	// ROE(자기 자본 이익률) // 순이익x100 / ((기초자기자본+기말자기자본)/2)
-	public Double getReturnOnEquity(LocalDateTime sDate, LocalDateTime eDate) {
-		Double a=financialStatementService.getNetProfit(sDate, eDate);
-		Double b=((financialStatementService.getTotalCapital(sDate) + financialStatementService.getTotalCapital(eDate))/2);
+	// 차트.자본&부채 비율 // (테스트 필요)
+	public String getCapitalLiabilityRatio (LocalDateTime sDate, LocalDateTime eDate) {
+		Map<String, Double> capitalLiabilityRatio = new HashMap<>();
+		capitalLiabilityRatio.put("Capital", financialStatementService.getTotalCapital(getQuartersService.getPreviousQuartersDate()[1]));
+		capitalLiabilityRatio.put("Libility", financialStatementService.getTotalLiabilities(getQuartersService.getPreviousQuartersDate()[1]));
 		
-		return a*100/b;
+		Gson gson = new Gson();
+	    return gson.toJson(capitalLiabilityRatio);
 	}
 	
-	// ROA(총 자산 이익률) // 순이익x100 / ((기초총자산+기말총자산)/2)
-	public Double getReturnOnAssets(LocalDateTime sDate, LocalDateTime eDate) {
-		Double a=financialStatementService.getNetProfit(sDate, eDate);
-		Double b=(financialStatementService.getTotalAssets(sDate)+financialStatementService.getTotalAssets(eDate))/2;
-		
-		return a*100/b;
-	}
-
 	
 	/*
 	@Test
